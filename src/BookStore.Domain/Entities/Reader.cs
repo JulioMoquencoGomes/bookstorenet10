0184d11c892a70b62eee111a95fbfa4823bb1d0a
JulioMoquencoGomes <mokenco2015@gmail.com>
2026-05-26 21:37:25 -0300

    (fix) sending data need from front

--- src/BookStore.Domain/Entities/Reader.cs
+++ src/BookStore.Domain/Entities/Reader.cs
@@ -7,14 +7,12 @@ public class Reader: TrackableEntity
     public Guid Id { get; set; }
     [Required]
     [MinLength(1, ErrorMessage = "ColumnName cannot be empty.")]
-    [RegularExpression(@"^\S+$")]
     public String Name { get; set; }
 
     public DateTime? Birthday { get; set; }
 
     [Required]
     [MinLength(1, ErrorMessage = "ColumnName cannot be empty.")]
-    [RegularExpression(@"^\S+$")]
     public String Urlimg { get; set; }
     
     public Reader(Guid id, String name, DateTime? birthday, String urlimg)
